f2a6bac3564d897ecfa07379fd302675c4e40094
AnnaKazDev <anna.kazmierczak.it@gmail.com>
2025-10-19 22:39:32 +0200

    fix(supabase): update DEFAULT_USER_ID to new value for user identification

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -4,7 +4,7 @@ import type { Database } from '../db/database.types.ts';
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
-export const DEFAULT_USER_ID = 'e3b50950-7881-4983-8888-63d3f5ea455d';
+export const DEFAULT_USER_ID = 'e9d12995-1f3e-491d-9628-3c4137d266d1';
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
 
